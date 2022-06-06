04888b7aea1adc1cc4eaf2963e63f490a3c012ab
BISINIGISHIVANI <bisinigishivani@gmail.com>
2022-06-06 12:57:36 +0530

    refactor:remove default export navbar,footer, and use prettier

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,23 +1,24 @@
+import { Link } from "react-router-dom";
 import hero from "../../assets/images/business-360.png";
-import { Footer } from "../../components/footer/footer";
-import { Navbar } from "../../components/navbar/navbar";
 export const Home = () => {
   return (
-  <div>
-    <Navbar/>
-    <div className="flex-row flex-wrap justify-center flex-center mg-sm">
-      <div className="padding-md flex-col width-xl">
-        <h2 className="padding-md">The Pomodoro Technique</h2>
-        <p className="width-sm">
-          Beat procrastination and improve your focus one pomodoro at a time.
-        </p>
-        <button className="primary-btn mg-sm text-center">Get started</button>
-      </div>
-      <div className="hero-img">
-        <img className="img-responsive" src={hero} alt="banner" />
+    <div>
+      <div className="flex-row flex-wrap justify-center flex-center mg-sm">
+        <div className="padding-md flex-col width-xl">
+          <h2 className="padding-md">The Pomodoro Technique</h2>
+          <p className="width-sm">
+            Beat procrastination and improve your focus one pomodoro at a time.
+          </p>
+          <Link to="/tasks">
+            <button className="primary-btn mg-sm cursor-pointer">
+              Get Started
+            </button>
+          </Link>
+        </div>
+        <div className="hero-img">
+          <img className="img-responsive" src={hero} alt="banner" />
+        </div>
       </div>
     </div>
-    <Footer/>
-  </div>
   );
 };
